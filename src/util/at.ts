712c8dab73ca1c6fefa9347baa9e37e075ff5297
Lars Kappert <lars@webpro.nl>
2023-03-27 13:36:41 +0200

    Expect TS issue

--- src/util/at.ts
+++ src/util/at.ts
@@ -1,4 +1,5 @@
 if (!('at' in Array.prototype)) {
+  // @ts-expect-error Array.prototype.at from Node.js v16.6.0
   Array.prototype.at = function (index) {
     if (index >= 0) return this[index];
     return this[this.length + index];
